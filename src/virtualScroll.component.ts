cc559e8e37874b981b798e0b2733e1eb936edefb
dinony <contact@dinony.com>
2017-06-27 15:01:32 +0200

    Remove unused operator

--- src/virtualScroll.component.ts
+++ src/virtualScroll.component.ts
@@ -21,7 +21,6 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/concatMap';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/pairwise';
