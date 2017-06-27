40ec4f03b35a2f08563b6eb72b64104cd304c156
dinony <contact@dinony.com>
2017-06-27 15:00:46 +0200

    Define user commands

---
+++ src/userCmd.ts
@@ -0,0 +1,25 @@
+
+export enum UserCmdOption {
+  SetScrollTop,
+  FocusRow,
+  FocusItem
+};
+
+export interface IUserCmd {
+  cmdType: UserCmdOption;
+}
+
+export class SetScrollTopCmd implements IUserCmd {
+  cmdType = UserCmdOption.SetScrollTop;
+  constructor(public value: number) {}
+}
+
+export class FocusRowCmd implements IUserCmd {
+  cmdType = UserCmdOption.FocusRow;
+  constructor(public rowIndex: number) {}
+}
+
+export class FocusItemCmd implements IUserCmd {
+  cmdType = UserCmdOption.FocusItem;
+  constructor(public itemIndex: number) {}
+}
