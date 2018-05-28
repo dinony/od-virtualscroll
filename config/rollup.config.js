b81c89d97e335edefbdb547f701bb737e5cef836
Onur D <contact@dinony.com>
2018-05-28 18:09:44 +0200

    Adapt new rollup format

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,17 +1,19 @@
 export default {
-  entry: 'dist/es5/api.js',
-  dest: 'dist/bundle/od-virtualscroll.umd.js',
-  sourceMap: true,
-  format: 'umd',
-  moduleName: 'od.virtualscroll',
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    // 'rxjs/Observable': 'Rx',
-    // 'rxjs/ReplaySubject': 'Rx',
-    // 'rxjs/add/operator/map': 'Rx.Observable.prototype',
-    // 'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-    // 'rxjs/add/observable/fromEvent': 'Rx.Observable',
-    // 'rxjs/add/observable/of': 'Rx.Observable'
+  input: 'dist/es5/api.js',
+  output: {
+    name: 'od.virtualscroll',
+    format: 'umd',
+    file: 'dist/bundle/od-virtualscroll.umd.js',
+    sourcemap: true,
+    globals: {
+      '@angular/core': 'ng.core',
+      '@angular/common': 'ng.common',
+      // 'rxjs/Observable': 'Rx',
+      // 'rxjs/ReplaySubject': 'Rx',
+      // 'rxjs/add/operator/map': 'Rx.Observable.prototype',
+      // 'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+      // 'rxjs/add/observable/fromEvent': 'Rx.Observable',
+      // 'rxjs/add/observable/of': 'Rx.Observable'
+    }
   }
 }
