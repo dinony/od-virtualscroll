fb35c85589e4b279204723b934ec5624a34afa14
Onur D <contact@dinony.com>
2018-06-11 16:03:36 +0200

    Adapt demo rollup config

--- config/rollup-demo.config.js
+++ config/rollup-demo.config.js
@@ -1,24 +1,14 @@
-import rollup from 'rollup'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify'
+import {uglify} from 'rollup-plugin-uglify'
 import {minify} from 'uglify-js-harmony';
 
 export default {
-  entry: 'buildTmp/demo/src/main-aot.js',
-  dest: 'build/build.gen.js', // output a single application bundle
-  sourceMap: true,
-  format: 'iife',
-  onwarn: function(warning) {
-    // Skip certain warnings
-
-    // should intercept ... but doesn't in some rollup versions
-    if ( warning.code === 'THIS_IS_UNDEFINED' ) { return; }
-    // intercepts in some rollup versions
-    if ( warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1 ) { return; }
-
-    // console.warn everything else
-    console.warn( warning.message );
+  input: 'buildTmp/demo/src/main-aot.js',
+  output: {
+    file: 'build/build.gen.js', // output a single application bundle
+    sourcemap: true,
+    format: 'iife'
   },
   plugins: [
     nodeResolve({jsnext: true, module: true}),
