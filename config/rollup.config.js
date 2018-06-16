2a5c5c7910bebcd7b14c6921b822449ac0d118f0
Onur D <contact@dinony.com>
2018-06-16 11:38:40 +0200

    Add minified umd output

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,16 +1,40 @@
-export default {
-  input: 'dist/es5/api.js',
-  external: ['@angular/core', '@angular/common', 'rxjs', 'rxjs/operators'],
-  output: {
-    name: 'od.virtualscroll',
-    format: 'umd',
-    file: 'dist/bundle/od-virtualscroll.umd.js',
-    sourcemap: true,
-    globals: {
-      '@angular/core': 'ng.core',
-      '@angular/common': 'ng.common',
-      'rxjs': 'rxjs',
-      'rxjs/operators': 'rxjs.operators',
+import {uglify} from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
+
+export default [
+  {
+    input: 'dist/es5/api.js',
+    external: ['@angular/core', '@angular/common', 'rxjs', 'rxjs/operators'],
+    output: {
+      name: 'od.virtualscroll',
+      format: 'umd',
+      file: 'dist/bundle/od-virtualscroll.umd.js',
+      sourcemap: true,
+      globals: {
+        '@angular/core': 'ng.core',
+        '@angular/common': 'ng.common',
+        'rxjs': 'rxjs',
+        'rxjs/operators': 'rxjs.operators',
+      }
+    },
+  },
+  {
+    input: 'dist/es5/api.js',
+    external: ['@angular/core', '@angular/common', 'rxjs', 'rxjs/operators'],
+    plugins: [
+      uglify(),
+      filesize()
+    ],
+    output: {
+      name: 'od.virtualscroll',
+      format: 'umd',
+      file: 'dist/bundle/od-virtualscroll.min.umd.js',
+      sourcemap: true,
+      globals: {
+        '@angular/core': 'ng.core',
+        '@angular/common': 'ng.common',
+        'rxjs': 'rxjs',
+        'rxjs/operators': 'rxjs.operators',
+      }
     }
-  }
-}
+  }]
